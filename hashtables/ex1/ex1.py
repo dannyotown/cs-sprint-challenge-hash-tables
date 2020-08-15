aaaccc2c1463571704ead7d5d713497b931f7be1
Dtown <dannyotown@gmail.com>
2020-08-15 12:26:32 -0500

    ex3 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,7 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
     return None
+
+
+get_indices_of_item_weights([12, 6, 7, 14, 19, 3, 0, 25, 40], 9, 7)
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,6 +4,9 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+    def __str__(self):
+        return f'source: {self.source}, destination: {self.destination}'
+
 
 def reconstruct_trip(tickets, length):
     """
@@ -11,4 +14,22 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
-    return route
+    d = {}
+    arr = []
+    for ticket in tickets:
+        d[ticket.source] = ticket.destination
+    arr.insert(0, d['NONE'])
+    count = 0
+    while len(arr) < length:
+        arr.append(d[arr[count]])
+        count += 1
+    return arr
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+reconstruct_trip(tickets, 3)
+expected = ["PDX", "DCA", "NONE"]
