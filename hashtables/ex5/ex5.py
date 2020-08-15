68c6bcf7a0b3d7d23f4742cce3804c0cac78f963
Dtown <dannyotown@gmail.com>
2020-08-15 11:53:36 -0500

    ex5 done

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,8 +6,19 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
-    print(files)
-    print(queries)
+    d = dict()
+    for path in files:
+        file = path.split('/')
+        file_in_path = file[-1:][0]
+        if file_in_path in d:
+            d[file_in_path] = d[file_in_path] + [path]
+        else:
+            d[file_in_path] = [path]
+    arr = []
+    for value in queries:
+        if value in d:
+            arr = arr + d[value]
+    return arr
 
 
 if __name__ == "__main__":
